a9374ba6c22d154d43b9ad6ae250326a506ba673
Tarvico <jeffhonforloco@gmail.com>
2026-09-16 20:00:56 -0400

    Revert temp error-details change

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -62,7 +62,7 @@ class ErrorBoundary extends Component<Props, State> {
             >
               Refresh Page
             </button>
-            {true && this.state.error && (
+            {import.meta.env.DEV && this.state.error && (
               <details className="mt-6 text-left">
                 <summary className="text-gray-400 cursor-pointer">Error Details</summary>
                 <pre className="mt-2 p-4 bg-gray-900 text-red-400 text-sm rounded overflow-auto">
